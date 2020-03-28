76b1d8a3ab5f71ff09c5863b036435172040f571
pankhuri-62 <62798803+pankhuri-62@users.noreply.github.com>
2020-03-28 18:59:22 +0530

    Update MyMain.java

--- src/problem2/main/MyMain.java
+++ src/problem2/main/MyMain.java
@@ -8,4 +8,40 @@ package problem2.main;
 // executable class
 // use problem1.mybst.MyBinarySearchTree class for creating binary tree
 public class MyMain {
+  public static void preorder(TreeNode root){
+        if(root == null)    return;
+        else{
+            System.out.print(root.getData()+"\t");
+            preorder(root.getLeft());
+            preorder(root.getRight());
+
+        }
+    }
+
+    public static void postorder(TreeNode root){
+        if(root == null)    return;
+        else{
+            postorder(root.getLeft());
+            postorder(root.getRight());
+            System.out.print(root.getData()+"\t");
+        }
+    }
+
+    public static void prepost(){
+        MyBinarySearchTree obj = new MyBinarySearchTree();
+        postorder(obj.getRoot());
+        System.out.println();
+        preorder(obj.getRoot());
+    }
+    public static void main(String[] args) {
+        MyBinarySearchTree obj = new MyBinarySearchTree();
+        obj.insert(85);
+        obj.insert(63);
+        obj.insert(54);
+        obj.insert(2);
+        obj.insert(23);
+        obj.insert(95);
+        obj.insert(36);
+        prepost();
+    }
 }
